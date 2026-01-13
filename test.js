232fce143a6bc24df9bfd9a72c80839dfb67d452
aa01076167528@gmail.com <aa01076167528gmail.com@aa01076167528gmailcomui-MacBookPro.local>
2026-01-13 22:23:05 +0900

    js

---
+++ test.js
@@ -0,0 +1,396 @@
+// function myfunction(){
+//     var insidevar = "hello";
+//     console.log(insidevar);
+// }
+
+// myfunction();
+// console.log(insidevar);
+
+// var outsidevar = "hi"
+
+// function myfunction() {
+//     console.log(outsidevar)
+// }
+// myfunction();
+// console.log(outsidevar);
+
+// let age = 30;
+
+// if (true){
+//     let age = 20;
+//     console.log(age);
+// }
+// console.log(age);
+
+// const pi = 3.14159265359
+
+// console.log(x);
+// var x =5;
+
+// sayhello(); 
+
+// function sayhello() {
+//     console.log("hello, world!");
+// }
+
+// let integernumber = 42;
+// let floatingpointnumber = 3.14;
+
+// let singlequotes ='hello world';
+// let doublequotes ="javascript";
+// let backticks = 'welcome to |{doublequotes}';
+
+// const name = "alice"
+// const age = 30;
+// const greeting = '안녕하세요 |{name}님 나이는 |{age} 세 입니다.';
+// console.log(greeting);
+
+// const multilinestring = '
+// 첫 번째 줄
+// 두 번째 줄
+// 세 번째 줄';
+// console.log(multilinestring);
+
+// function customtag(stringe, ...values){
+//     let result = "";
+//     for (let i = 0; i < stringe.length; i++){
+//         result += stringe[i];
+//         if (i < values.length){
+//             result += values[i];
+//         }
+//     }
+//     return result
+// }
+// const name = "alice";
+// const age = 30;
+// const message = customtag'안녕하세요 |{name}님 나이는 |{age} 세 입니다' ;
+// console.log(message); 
+
+// let istrue = true;
+// let isfalse = false;
+
+// let emptyvalue = null;
+
+// let notdefined;
+// console.log(notdefined);
+
+// const uniqusesybol = Symbol('description');
+
+// const person = {
+//     name: "johon",
+//     age: 30,
+//     isstudent: false
+// };
+// console.log(person.name);
+
+// const numbels = [1,2,3,4,5];
+// console.log(numbels[0]); 
+
+// class person {
+//     constructor(name,age){
+//         this.name = name;
+//         this.age = age;
+
+
+//     }
+// }
+// const person1 = new person("alice",25);
+// const person2 = new person("bob",30);
+
+// const sayhello = function() {
+//     console.log("hello");
+// };
+// const greet = sayhello;
+// greet();
+
+// function operate(operate, x, y){
+//     return operate(x,y);
+// }
+// function add (a,b) {
+//     return a+b;
+// }
+// const result = operate(add, 5, 3);
+// console.log(result);
+
+// function createmultiplier(factor){
+//     return function(x){
+//         return x * factor;
+//     };
+// }
+// const double = createmultiplier(2);
+// const triple = createmultiplier(3);
+// console.log(double(5));
+// console.log(triple(5));
+
+// const person = {
+//     name: "alice",
+//     sayhello: function() {
+//         console.log('hello,|{this.name}!');
+//     }
+// };
+// person.sayhello(); 
+
+// function myfunction(){
+//     var insidevar = "hello";
+//     console.log(insidevar);
+// }
+
+// myfunction();
+// console.log(insidevar);
+
+// var outsidevar = "hi"
+
+// function myfunction() {
+//     console.log(outsidevar)
+// }
+// myfunction();
+// console.log(outsidevar);
+
+// let age = 30;
+
+// if (true){
+//     let age = 20;
+//     console.log(age);
+// }
+// console.log(age);
+
+// const pi = 3.14159265359
+
+// console.log(x);
+// var x =5;
+
+// sayhello(); 
+
+// function sayhello() {
+//     console.log("hello, world!");
+// }
+
+// let integernumber = 42;
+// let floatingpointnumber = 3.14;
+
+// let singlequotes ='hello world';
+// let doublequotes ="javascript";
+// let backticks = 'welcome to |{doublequotes}';
+
+// const name = "alice"
+// const age = 30;
+// const greeting = '안녕하세요 |{name}님 나이는 |{age} 세 입니다.';
+// console.log(greeting);
+
+// const multilinestring = '
+// 첫 번째 줄
+// 두 번째 줄
+// 세 번째 줄';
+// console.log(multilinestring);
+
+// function customtag(stringe, ...values){
+//     let result = "";
+//     for (let i = 0; i < stringe.length; i++){
+//         result += stringe[i];
+//         if (i < values.length){
+//             result += values[i];
+//         }
+//     }
+//     return result
+// }
+// const name = "alice";
+// const age = 30;
+// const message = customtag'안녕하세요 |{name}님 나이는 |{age} 세 입니다' ;
+// console.log(message); 
+
+// let istrue = true;
+// let isfalse = false;
+
+// let emptyvalue = null;
+
+// let notdefined;
+// console.log(notdefined);
+
+// const uniqusesybol = Symbol('description');
+
+// const person = {
+//     name: "johon",
+//     age: 30,
+//     isstudent: false
+// };
+// console.log(person.name);
+
+// const numbels = [1,2,3,4,5];
+// console.log(numbels[0]); 
+
+// class person {
+//     constructor(name,age){
+//         this.name = name;
+//         this.age = age;
+
+
+//     }
+// }
+// const person1 = new person("alice",25);
+// const person2 = new person("bob",30);
+
+// const sayhello = function() {
+//     console.log("hello");
+// };
+// const greet = sayhello;
+// greet();
+
+// function operate(operate, x, y){
+//     return operate(x,y);
+// }
+// function add (a,b) {
+//     return a+b;
+// }
+// const result = operate(add, 5, 3);
+// console.log(result);
+
+// function createmultiplier(factor){
+//     return function(x){
+//         return x * factor;
+//     };
+// }
+// const double = createmultiplier(2);
+// const triple = createmultiplier(3);
+// console.log(double(5));
+// console.log(triple(5));
+
+// const person = {
+//     name: "alice",
+//     sayhello: function() {
+//         console.log('hello,|{this.name}!');
+//     }
+// };
+// person.sayhello(); 
+
+// function myfunction(){
+//     var insidevar = "hello";
+//     console.log(insidevar);
+// }
+
+// myfunction();
+// console.log(insidevar);
+
+// var outsidevar = "hi"
+
+// function myfunction() {
+//     console.log(outsidevar)
+// }
+// myfunction();
+// console.log(outsidevar);
+
+// let age = 30;
+
+// if (true){
+//     let age = 20;
+//     console.log(age);
+// }
+// console.log(age);
+
+// const pi = 3.14159265359
+
+// console.log(x);
+// var x =5;
+
+// sayhello(); 
+
+// function sayhello() {
+//     console.log("hello, world!");
+// }
+
+// let integernumber = 42;
+// let floatingpointnumber = 3.14;
+
+// let singlequotes ='hello world';
+// let doublequotes ="javascript";
+// let backticks = 'welcome to |{doublequotes}';
+
+// const name = "alice"
+// const age = 30;
+// const greeting = '안녕하세요 |{name}님 나이는 |{age} 세 입니다.';
+// console.log(greeting);
+
+// const multilinestring = '
+// 첫 번째 줄
+// 두 번째 줄
+// 세 번째 줄';
+// console.log(multilinestring);
+
+// function customtag(stringe, ...values){
+//     let result = "";
+//     for (let i = 0; i < stringe.length; i++){
+//         result += stringe[i];
+//         if (i < values.length){
+//             result += values[i];
+//         }
+//     }
+//     return result
+// }
+// const name = "alice";
+// const age = 30;
+// const message = customtag'안녕하세요 |{name}님 나이는 |{age} 세 입니다' ;
+// console.log(message); 
+
+// let istrue = true;
+// let isfalse = false;
+
+// let emptyvalue = null;
+
+// let notdefined;
+// console.log(notdefined);
+
+// const uniqusesybol = Symbol('description');
+
+// const person = {
+//     name: "johon",
+//     age: 30,
+//     isstudent: false
+// };
+// console.log(person.name);
+
+// const numbels = [1,2,3,4,5];
+// console.log(numbels[0]); 
+
+// class person {
+//     constructor(name,age){
+//         this.name = name;
+//         this.age = age;
+
+
+//     }
+// }
+// const person1 = new person("alice",25);
+// const person2 = new person("bob",30);
+
+// const sayhello = function() {
+//     console.log("hello");
+// };
+// const greet = sayhello;
+// greet();
+
+// function operate(operate, x, y){
+//     return operate(x,y);
+// }
+// function add (a,b) {
+//     return a+b;
+// }
+// const result = operate(add, 5, 3);
+// console.log(result);
+
+// function createmultiplier(factor){
+//     return function(x){
+//         return x * factor;
+//     };
+// }
+// const double = createmultiplier(2);
+// const triple = createmultiplier(3);
+// console.log(double(5));
+// console.log(triple(5));
+
+// const person = {
+//     name: "alice",
+//     sayhello: function() {
+//         console.log('hello,|{this.name}!');
+//     }
+// };
+// person.sayhello(); 
+
---
+++ test2.js
@@ -0,0 +1,762 @@
+// const firstname = "ailce";
+// const lastname = "johnson";
+// const fullname = firstname + "" +lastname;
+// console.log(fullname);
+
+// const text = "hello world"
+// console.log(text.length)
+
+// const text ="hello world";
+// console.log(text[0]);
+// console.log(text[7]);
+
+// const text = "Hello, World!";
+// console.log(text.substring(0, 5)); // "Hello" 출력
+// console.log(text.slice(7, 12)); // "World" 출력
+
+// const text = "Hello, World!";
+// console.log(text.indexOf("World")); // 7 출력
+// console.log(text.includes("Hello")); // true 출력
+// console.log(text.startsWith("Hello")); // true 출력
+// console.log(text.endsWith("!")); // true 출력
+
+
+// const text = "hello world";
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
+
+// const text = "apple,banana,cherry";
+// const fruits = text.split(",");
+// console.log(fruits);
+
+// const a = 5;
+// const b = 3;
+// const sum = a+b;
+// console.log(sum);
+
+// const a = 10;
+// const b = 4;
+// const sum = a-b;
+// console.log(sum);
+
+// const a = 7;
+// const b = 6;
+// const sum = a*b;
+// console.log(sum);
+
+// const a = 20;
+// const b = 5;
+// const sum = a/b;
+// console.log(sum);
+
+// const a = 17;
+// const b = 5;
+// const sum = a%b;
+// console.log(sum);
+
+// const a = 10007;
+// const b = 35;
+// const sum = a ** b;
+// console.log(sum);
+
+// let x = 5;
+// let y = ++x;
+// console.log(x);
+// console.log(y);
+
+// let x = 10;
+// let y = x++;
+// console.log(x);
+// console.log(y);
+
+// let x = 10;
+
+// let x =5
+// x += 3;
+
+// let y = 7;
+// y -= 2;
+
+// let z = 3;
+// z *= 4; 
+
+// let a = 20;
+// a /= 5;
+
+// let b = 17;
+// b %= 3; 
+
+// let c = 2;
+// c **= 3; 
+
+// const x = true;
+// const y = false;
+
+// const result1 = x && y; 
+// const result2 = x && (5 > 2); 
+
+// const a = true;
+// const b = false;
+
+// const result1 = a || b; 
+// const result2 = (3 < 1) || (2 == 2); 
+// console.log(result1);
+// console.log(result2);
+
+// const value1 = true;
+// const value2 = false;
+
+// const result1 = !value1; 
+// const result2 = !(10 > 5); 
+
+// console.log(result1);
+// console.log(result2);
+
+// const num1 = 5;
+// const num2 = "5";
+
+// console.log(num1 == num2);
+
+// const num1 = 5;
+// const num2 = "5";
+
+// console.log(num1 === num2); 
+
+// const x = 10;
+// const y = 20;
+
+// console.log(x != y);
+
+// const a = "Hello";
+// const b = "World";
+
+// console.log(a !== b); 
+
+// const m = 15;
+// const n = 10;
+
+// console.log(m > n); 
+// console.log(n < m); 
+// console.log(m >= n); 
+// console.log(n <= m); 
+
+// (condition) ? value_if_true : value_if_false;
+
+// const age = 20;
+// const canVote = (age >= 18) ? "투표 가능" : "투표 불가능";
+// console.log(canVote); 
+
+
+// let fTemp = 122;
+// let cTemp = (fTemp - 32) * (5/9);
+
+// console.log(cTemp);
+
+// let num1 = 456;
+// let num2 = 111;
+
+
+// num1 = num1 - (num1 % 100);
+// num2 = num2 - (num2 % 100);
+
+// console.log(num1); 
+// console.log(num2); 
+
+// let num = 12345;
+
+
+// const digit1 = num % 10; 
+// num = Math.floor(num / 10); 
+// const digit2 = num % 10; 
+// num = Math.floor(num / 10);
+// const digit3 = num % 10;
+// num = Math.floor(num / 10);
+// const digit4 = num % 10;
+// num = Math.floor(num / 10); 
+// const digit5 = num; 
+
+// const sum = digit1 + digit2 + digit3 + digit4 + digit5;
+
+// console.log(sum); 
+
+// const firstname = "ailce";
+// const lastname = "johnson";
+// const fullname = firstname + "" +lastname;
+// console.log(fullname);
+
+// const text = "hello world"
+// console.log(text.length)
+
+// const text ="hello world";
+// console.log(text[0]);
+// console.log(text[7]);
+
+// const text = "Hello, World!";
+// console.log(text.substring(0, 5)); // "Hello" 출력
+// console.log(text.slice(7, 12)); // "World" 출력
+
+// const text = "Hello, World!";
+// console.log(text.indexOf("World")); // 7 출력
+// console.log(text.includes("Hello")); // true 출력
+// console.log(text.startsWith("Hello")); // true 출력
+// console.log(text.endsWith("!")); // true 출력
+
+
+// const text = "hello world";
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
+
+// const text = "apple,banana,cherry";
+// const fruits = text.split(",");
+// console.log(fruits);
+
+// const a = 5;
+// const b = 3;
+// const sum = a+b;
+// console.log(sum);
+
+// const a = 10;
+// const b = 4;
+// const sum = a-b;
+// console.log(sum);
+
+// const a = 7;
+// const b = 6;
+// const sum = a*b;
+// console.log(sum);
+
+// const a = 20;
+// const b = 5;
+// const sum = a/b;
+// console.log(sum);
+
+// const a = 17;
+// const b = 5;
+// const sum = a%b;
+// console.log(sum);
+
+// const a = 10007;
+// const b = 35;
+// const sum = a ** b;
+// console.log(sum);
+
+// let x = 5;
+// let y = ++x;
+// console.log(x);
+// console.log(y);
+
+// let x = 10;
+// let y = x++;
+// console.log(x);
+// console.log(y);
+
+// let x = 10;
+
+// let x =5
+// x += 3;
+
+// let y = 7;
+// y -= 2;
+
+// let z = 3;
+// z *= 4; 
+
+// let a = 20;
+// a /= 5;
+
+// let b = 17;
+// b %= 3; 
+
+// let c = 2;
+// c **= 3; 
+
+// const x = true;
+// const y = false;
+
+// const result1 = x && y; 
+// const result2 = x && (5 > 2); 
+
+// const a = true;
+// const b = false;
+
+// const result1 = a || b; 
+// const result2 = (3 < 1) || (2 == 2); 
+// console.log(result1);
+// console.log(result2);
+
+// const value1 = true;
+// const value2 = false;
+
+// const result1 = !value1; 
+// const result2 = !(10 > 5); 
+
+// console.log(result1);
+// console.log(result2);
+
+// const num1 = 5;
+// const num2 = "5";
+
+// console.log(num1 == num2);
+
+// const num1 = 5;
+// const num2 = "5";
+
+// console.log(num1 === num2); 
+
+// const x = 10;
+// const y = 20;
+
+// console.log(x != y);
+
+// const a = "Hello";
+// const b = "World";
+
+// console.log(a !== b); 
+
+// const m = 15;
+// const n = 10;
+
+// console.log(m > n); 
+// console.log(n < m); 
+// console.log(m >= n); 
+// console.log(n <= m); 
+
+// (condition) ? value_if_true : value_if_false;
+
+// const age = 20;
+// const canVote = (age >= 18) ? "투표 가능" : "투표 불가능";
+// console.log(canVote); 
+
+
+// let fTemp = 122;
+// let cTemp = (fTemp - 32) * (5/9);
+
+// console.log(cTemp);
+
+// let num1 = 456;
+// let num2 = 111;
+
+
+// num1 = num1 - (num1 % 100);
+// num2 = num2 - (num2 % 100);
+
+// console.log(num1); 
+// console.log(num2); 
+
+// let num = 12345;
+
+
+// const digit1 = num % 10; 
+// num = Math.floor(num / 10); 
+// const digit2 = num % 10; 
+// num = Math.floor(num / 10);
+// const digit3 = num % 10;
+// num = Math.floor(num / 10);
+// const digit4 = num % 10;
+// num = Math.floor(num / 10); 
+// const digit5 = num; 
+
+// const sum = digit1 + digit2 + digit3 + digit4 + digit5;
+
+// console.log(sum); 
+
+// const firstname = "ailce";
+// const lastname = "johnson";
+// const fullname = firstname + "" +lastname;
+// console.log(fullname);
+
+// const text = "hello world"
+// console.log(text.length)
+
+// const text ="hello world";
+// console.log(text[0]);
+// console.log(text[7]);
+
+// const text = "Hello, World!";
+// console.log(text.substring(0, 5)); // "Hello" 출력
+// console.log(text.slice(7, 12)); // "World" 출력
+
+// const text = "Hello, World!";
+// console.log(text.indexOf("World")); // 7 출력
+// console.log(text.includes("Hello")); // true 출력
+// console.log(text.startsWith("Hello")); // true 출력
+// console.log(text.endsWith("!")); // true 출력
+
+
+// const text = "hello world";
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
+
+// const text = "apple,banana,cherry";
+// const fruits = text.split(",");
+// console.log(fruits);
+
+// const a = 5;
+// const b = 3;
+// const sum = a+b;
+// console.log(sum);
+
+// const a = 10;
+// const b = 4;
+// const sum = a-b;
+// console.log(sum);
+
+// const a = 7;
+// const b = 6;
+// const sum = a*b;
+// console.log(sum);
+
+// const a = 20;
+// const b = 5;
+// const sum = a/b;
+// console.log(sum);
+
+// const a = 17;
+// const b = 5;
+// const sum = a%b;
+// console.log(sum);
+
+// const a = 10007;
+// const b = 35;
+// const sum = a ** b;
+// console.log(sum);
+
+// let x = 5;
+// let y = ++x;
+// console.log(x);
+// console.log(y);
+
+// let x = 10;
+// let y = x++;
+// console.log(x);
+// console.log(y);
+
+// let x = 10;
+
+// let x =5
+// x += 3;
+
+// let y = 7;
+// y -= 2;
+
+// let z = 3;
+// z *= 4; 
+
+// let a = 20;
+// a /= 5;
+
+// let b = 17;
+// b %= 3; 
+
+// let c = 2;
+// c **= 3; 
+
+// const x = true;
+// const y = false;
+
+// const result1 = x && y; 
+// const result2 = x && (5 > 2); 
+
+// const a = true;
+// const b = false;
+
+// const result1 = a || b; 
+// const result2 = (3 < 1) || (2 == 2); 
+// console.log(result1);
+// console.log(result2);
+
+// const value1 = true;
+// const value2 = false;
+
+// const result1 = !value1; 
+// const result2 = !(10 > 5); 
+
+// console.log(result1);
+// console.log(result2);
+
+// const num1 = 5;
+// const num2 = "5";
+
+// console.log(num1 == num2);
+
+// const num1 = 5;
+// const num2 = "5";
+
+// console.log(num1 === num2); 
+
+// const x = 10;
+// const y = 20;
+
+// console.log(x != y);
+
+// const a = "Hello";
+// const b = "World";
+
+// console.log(a !== b); 
+
+// const m = 15;
+// const n = 10;
+
+// console.log(m > n); 
+// console.log(n < m); 
+// console.log(m >= n); 
+// console.log(n <= m); 
+
+// (condition) ? value_if_true : value_if_false;
+
+// const age = 20;
+// const canVote = (age >= 18) ? "투표 가능" : "투표 불가능";
+// console.log(canVote); 
+
+
+// let fTemp = 122;
+// let cTemp = (fTemp - 32) * (5/9);
+
+// console.log(cTemp);
+
+// let num1 = 456;
+// let num2 = 111;
+
+
+// num1 = num1 - (num1 % 100);
+// num2 = num2 - (num2 % 100);
+
+// console.log(num1); 
+// console.log(num2); 
+
+// let num = 12345;
+
+
+// const digit1 = num % 10; 
+// num = Math.floor(num / 10); 
+// const digit2 = num % 10; 
+// num = Math.floor(num / 10);
+// const digit3 = num % 10;
+// num = Math.floor(num / 10);
+// const digit4 = num % 10;
+// num = Math.floor(num / 10); 
+// const digit5 = num; 
+
+// const sum = digit1 + digit2 + digit3 + digit4 + digit5;
+
+// console.log(sum); 
+
+// const age = 25;
+// if (age >= 18) {
+// 	console.log("성인");
+// }
+
+// const score = 85;
+// if (score >= 90) {
+//   console.log("A 학점");
+// } else if (score >= 80) {
+//   console.log("B 학점");
+// } else if (score >= 70) {
+//   console.log("C 학점");
+// } else {
+//   console.log("D 학점");
+// }
+
+// const israining = true
+// if (israining) {
+//     console.log("비가 옵니다.")
+// } else
+// console.log("비가오지 않습니다")
+
+// const age =25;
+// const haslicense = true;
+// if (age <=28){
+//     console.log("성인");
+// if (haslicense){
+//     console.log("운전면허O")
+// } else{
+//     console.log("운전면허X")
+// }
+// } else{
+//     console.log("민짜")
+// }
+
+// switch (표현식) {
+//     case 값1:
+     
+//       break;
+//     case 값2:
+      
+//       break;
+    
+//     default:
+      
+//   }
+// const date = new Date();
+// let hour = date.getHours();
+// let min = date.getMinutes();
+// let sec = date.getSeconds();
+
+// console.log("현재시각 : " + hour + "시 " + min + "분 " + sec + "초");
+
+// let timeOfDay;
+
+// if (hour >= 6 && hour < 12) {
+//   timeOfDay = "아침";
+// } else if (hour >= 12 && hour < 18) {
+//   timeOfDay = "점심";
+// } else {
+//   timeOfDay = "저녁";
+// }
+
+// console.log("지금은 " + timeOfDay + "입니다.");
+// let input = Number(prompt('숫자를 입력해주세요', '숫자'));
+// switch (input % 2) {
+// case 0:
+// alert('짝수입니다.');
+// break;
+// case 1:
+// alert('홀수입니다.');
+// break;
+// default:
+// alert('숫자만 입력해주세요.');
+// }
+
+// while(조건) {
+    
+// }
+
+//  let count =1;
+
+// while (count<=5 ){
+//     if (count %2===0){
+//         count++;
+//         coutinue;
+//     }
+//     console.log("홀수:"+ count);
+//     count++;
+// }
+
+// for(초기화; 조건; 반복후 작업){
+
+// // }
+
+// // for(let i = 1; i <=5; i++){
+// //     console.log(i);
+// // }
+// // for (외부 루프 초기화; 외부 루프 조건; 외부 루프 반복 후 작업) {
+// //     for (내부 루프 초기화; 내부 루프 조건; 내부 루프 반복 후 작업) {
+    
+// //     }
+    
+// //   }
+
+// // const matrix = [
+// //     [1,2,3]
+// //     [4,5,6]
+// //     [7,8,9]
+// // ];
+
+// // for (let i = 0; i<matrix.length;i++){
+// //     for (let j = 0; j< matrix[i],length; j++){
+// //         console.log(matrix[i][j]);
+// //     }
+// // }
+// const targetNumber = Math.floor(Math.random() * 10) + 1;
+// let attempts = 0; 
+
+// while (true) {
+//   const userGuess = prompt("1에서 10 사이의 숫자를 추측하세요.");
+//   const guess = parseInt(userGuess);
+
+//   if (isNaN(guess) || guess < 1 || guess > 10) {
+//     alert("올바른 숫자를 입력하세요.");
+//     continue; 
+//   }
+
+// //   attempts++;
+
+// //   if (guess === targetNumber) {
+// //     alert(`정답입니다! ${attempts}번 만에 맞추셨습니다.`);
+// //     break;
+// //   } else if (guess < targetNumber) {
+// //     alert("입력된 숫자가 더 작습니다.");
+// //   } else {
+// //     alert("입력된 숫자가 더 큽니다.");
+// //   }
+// // }
+// // let sum = 0; 
+
+// // for (let i = 1; i <= 100; i++) {
+// //   if (i % 2 === 0) {
+// //     
+// //     sum += i;
+// //   }
+// // }
+
+// // console.log("1에서 100 사이의 2의 배수 합: " + sum);
+// // const n = 5; 
+
+// // for (let i = 1; i <= n; i++) {
+// //   let stars = "";
+// //   for (let j = 1; j <= i; j++) {
+// //     stars += "*";
+// //   }
+// //   console.log(stars);
+// // }
+// // const n = 5; 
+
+// // for (let i = n; i >= 1; i--) {
+// //   let stars = "";
+// //   for (let j = 1; j <= i; j++) {
+// //     stars += "*";
+// //   }
+// //   console.log(stars);
+// // }
+
+// const age =25;
+// if (age >=18){
+//     console.log("성인입니다.")
+// }
+
+// const scoer =85;
+// if (scoer >=90){
+//     console.log("A학점");
+// } else if (scoer>=80){
+//     console.log("B학점");
+// } else if (scoer>=70){
+//     console.log("c학점");
+// }
+// else{
+//     console.log("D학점");
+// }
+// const israining= true;
+// if (israining){
+//     console.log("비가옵니다.");
+//  } else {
+//         console.log("날씨가 맑습니다.");
+//     }
+// const age = 25;
+// const haslicense = true;
+// if (age >= 18) {
+//     console.log("성인입니다.");
+//     if (haslicense){
+//         console.log("운전면허가 있습니다.");
+//     } else {
+//         console.log("운전면허가 없습니다.");
+//     } else {
+//         console.log("미성년자입니다");
+//     }
+// }
+
+// switch(표현식){
+//     case 값1:
+//         break;
+//      case 값2:
+//         break;
+//     default:
+// }
+
+// while(조건){
+
+// }
+// let count = 1;
+// while(count <=5){
+//     count++;
+//     continue
+// }
